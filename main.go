b1ffb3a88ecc72aa82cc421e383fe6113e13d894
IvzhenkoNazar <61440742+IvzhenkoNazar@users.noreply.github.com>
2020-02-26 13:49:23 +0300

    Create main.go

---
+++ main.go
@@ -0,0 +1,30 @@
+package main
+
+import (
+	"fmt"
+	"log"
+	"net/http"
+	"text/template"
+)
+
+func main() {
+	http.HandleFunc("/", mainPage)
+	port := ":9999"
+	fmt.Println("Server listen on port: ", port)
+	err := http.ListenAndServe(port, nil)
+	if err != nil {
+		log.Fatal("ListenAndServe", err)
+	}
+}
+
+func mainPage(w http.ResponseWriter, r *http.Request) {
+	tmpl, err := template.ParseFiles("static/index.html")
+	if err != nil {
+		http.Error(w, err.Error(), 400)
+		return
+	}
+	if err := tmpl.Execute(w, nil); err != nil {
+		http.Error(w, err.Error(), 400)
+		return
+	}
+}
